05c7cada52307500b37cd2fbbc1b710ae08c0fbd
IamPersistent <develop@zestic.com>
2011-12-21 02:14:04 -0800

    inital commit

---
+++ VespolinaCheckoutBundle.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * (c) 2011-2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\CheckoutBundle;
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VespolinaCheckoutBundle extends Bundle
+{
+
+}
