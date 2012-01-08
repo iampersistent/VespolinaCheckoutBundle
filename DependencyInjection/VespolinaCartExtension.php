19672ad80afe88a9270a1782e680a8422f779b10
IamPersistent <develop@zestic.com>
2012-01-07 17:10:38 -0800

    load configuration file

---
+++ DependencyInjection/VespolinaCartExtension.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\CheckoutBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+class VespolinaCheckoutExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load(sprintf('checkout.xml'));
+    }
+}
