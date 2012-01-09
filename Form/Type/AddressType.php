f1db15b95d0d857204ace3eab16baa952ff814a0
IamPersistent <develop@zestic.com>
2012-01-09 05:15:59 -0800

    adding form support for address

---
+++ Form/Type/AddressType.php
@@ -0,0 +1,53 @@
+<?php
+namespace Vespolina\CheckoutBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+
+class AddressType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('street1', 'textarea', array(
+                'required' => true,
+            ))
+            ->add('street2', 'textarea', array(
+                'required' => false,
+            ))
+            ->add('city', 'textarea', array(
+                'required' => true,
+            ))
+            ->add('state', 'vespolina_state', array(
+                'required' => true,
+            ))
+            ->add('country', 'country', array(
+                'required' => true,
+            ))
+            ->add('postalCode', 'textarea', array(
+                'required' => true,
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(array $options)
+    {
+        return 'field';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'vespolina_address';
+    }
+}
---
+++ Model/Address.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Vespolina\CheckoutBundle\Model;
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+
+class Address
+{
+    protected $street1;
+    protected $street2;
+    protected $city;
+    protected $state;
+    protected $country;
+    protected $postalCode;
+
+    public function setStreet1($street1)
+    {
+        $this->street1 = $street1;
+    }
+
+    public function getStreet1()
+    {
+        return $this->street1;
+    }
+
+    public function setStreet2($street2)
+    {
+        $this->street2 = $street2;
+    }
+
+    public function getStreet2()
+    {
+        return $this->street2;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+    }
+}
