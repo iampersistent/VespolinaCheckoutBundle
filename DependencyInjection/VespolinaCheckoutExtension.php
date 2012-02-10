b6c371d86d95a8a23dc5fa971ad0a76bfe681024
IamPersistent <develop@zestic.com>
2012-02-10 04:09:56 -0800

    handle configurations

--- DependencyInjection/VespolinaCheckoutExtension.php
+++ DependencyInjection/VespolinaCheckoutExtension.php
@@ -21,6 +21,10 @@ class VespolinaCheckoutExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load(sprintf('checkout.xml'));
